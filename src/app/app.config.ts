c8ab786530b94358bc75c21b25388bf9eb58bc28
Benjo D <benjo.freelancer@gmail.com>
2024-09-30 10:00:24 +0800

    reafactor: code update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 import { provideRouter } from '@angular/router';
+import { appReducers } from '@store/reducers';
+import { appEffects } from '@store/effects';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideHttpClient(withFetch()),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideStore(appReducers),
+    provideEffects(appEffects),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('@home/features').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'history',
+    loadComponent: () => import('@history').then((m) => m.HistoryComponent),
+    outlet: 'history',
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home(history:/history)',
+  },
+];
